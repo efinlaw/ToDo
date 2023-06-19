a20b80dc1b795f293cc7c9dee41cb501ca372c48
efinlaw <47069204+efinlaw@users.noreply.github.com>
2023-06-19 13:35:48 -0700

    declared variables for sidebar dom elements

---
+++ src/controllers/sidebarController.js
@@ -0,0 +1,8 @@
+// DOM elements
+const home = document.getElementById('home');
+const today = document.getElementById('today');
+const week = document.getElementById('week');
+const addProjButton = document.querySelector('.add-project');
+const newProjectInput = document.getElementById('project-input');
+const newProjectConfirm = document.querySelector('.confirm');
+const newProjectCancel = document.querySelector('.cancel');
